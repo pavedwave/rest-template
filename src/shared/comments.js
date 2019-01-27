8caf043b89f463763fa4861591a23e566af01e63
pavedwave <pavedwave@gmail.com>
2019-01-26 16:53:28 -0800

    New_Comments

--- src/shared/comments.js
+++ src/shared/comments.js
@@ -4,24 +4,24 @@ export const COMMENTS =
     id: 0,
     dishId: 0,
     rating: 5,
-    comment: "Tastes great!",
-    author: "John Lemon",
+    comment: "Great new casual spot with all the fab bartenders from Ravenna Ale House - bring your liver ....or not...food pretty good too ....unsure about following the crew from the move , but turned out to be well worth it",
+    author: "Roderic D.",
     date: "2012-10-16T17:57:28.556094Z"
     },
     {
     id: 1,
     dishId: 0,
-    rating: 4,
-    comment: "Less filling!",
-    author: "Paul McVites",
+    rating: 5,
+    comment: "The beer I had was awesome and service was friendly! I didn't get any food but it smelled super good and the happy hour prices were great. I'll be coming back!",
+    author: "Sofie B.",
     date: "2014-09-05T17:57:28.556094Z"
     },
     {
     id: 2,
     dishId: 0,
-    rating: 3,
+    rating: 4,
     comment: "Carnivores unite!",
-    author: "Michael Jaikishan",
+    author: "Michael J.",
     date: "2015-02-13T17:57:28.556094Z"
     },
     {
@@ -29,15 +29,15 @@ export const COMMENTS =
     dishId: 0,
     rating: 4,
     comment: "The answer to Where's the Beef!",
-    author: "Ringo Starry",
+    author: "R.S.",
     date: "2013-12-02T17:57:28.556094Z"
     },
     {
     id: 4,
     dishId: 0,
-    rating: 2,
-    comment: "It's your birthday, we're gonna party!",
-    author: "25 Cent",
+    rating: 4,
+    comment: "The Double J really fills a need in this neighborhood, it's a full service bar that you wouldn't be embarrassed to take out-of-town folks too. They have a full bar and a decent cocktail menu as well as your standard beer choices. I haven't tried their food yet but it smelled really good.  I look forward to going back and trying some.",
+    author: "C. H.",
     date: "2011-12-02T17:57:28.556094Z"
     },
     {
@@ -51,113 +51,113 @@ export const COMMENTS =
     {
     id: 6,
     dishId: 1,
-    rating: 4,
-    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-    author: "Paul McVites",
+    rating: 5,
+    comment: "We luuuuurv it here.  Laid-back, reasonably priced, great jukebox (download the Touchtunes app), and tasty bar food. Friendly staff. Why aren't you here right now? Get over here. But expect a divey atmosphere and no fanciness.",
+    author: "Sue N.",
     date: "2014-09-05T17:57:28.556094Z"
     },
     {
     id: 7,
     dishId: 1,
-    rating: 3,
-    comment: "Eat it, just eat it!",
-    author: "Michael Jaikishan",
+    rating: 4,
+    comment: "Great to have more options for a night out here on LCW!!",
+    author: "Randy P.",
     date: "2015-02-13T17:57:28.556094Z"
     },
     {
     id: 8,
     dishId: 1,
     rating: 4,
-    comment: "Ultimate, Reaching for the stars!",
-    author: "Ringo Starry",
+    comment: "A well-needed addition to Lake City.  Nice work fellas.",
+    author: "Jude",
     date: "2013-12-02T17:57:28.556094Z"
     },
     {
     id: 9,
     dishId: 1,
-    rating: 2,
-    comment: "It's your birthday, we're gonna party!",
-    author: "25 Cent",
+    rating: 5,
+    comment: "Great live music, cool that they have open mic sessions with some real talent!  Good prices on the drinks, friendly atmosphere.  As Ahnold would say, I'll be back!",
+    author: "J.P.",
     date: "2011-12-02T17:57:28.556094Z"
     },
     {
     id: 10,
     dishId: 2,
     rating: 5,
-    comment: "Imagine all the eatables, living in conFusion!",
-    author: "John Lemon",
+    comment: "A perfect neighborhood bar filled with board games, classic 80's and 90's music, and a pool table. I couldn't ask for more! Drinks were reasonably priced. I had a manhattan and it was one of the better ones I've had in awhile. People would sing along to the songs, but the music wasn't too loud. you could still carry a descent conversation. I'm looking forward to trying some of their food and watching football here with friends in the fall.",
+    author: "Kelly H.",
     date: "2012-10-16T17:57:28.556094Z"
     },
     {
     id: 11,
     dishId: 2,
-    rating: 4,
-    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-    author: "Paul McVites",
+    rating: 5,
+    comment: "Nice little spot to grab a drink and play some board games (games are provided by the bar). We tried the food a couple times and everything has been tasty so far, especially the nachos and the French dip.",
+    author: "Shan C.",
     date: "2014-09-05T17:57:28.556094Z"
     },
     {
     id: 12,
     dishId: 2,
-    rating: 3,
-    comment: "Eat it, just eat it!",
-    author: "Michael Jaikishan",
+    rating: 5,
+    comment: "This place is legit - cheap drinks, friendly/no fuss attitude, great decor and ambiance, delicious bar food and so much more! They have free peanuts, karaoke on Tuesdays I believe, themed food nights (i.e. Taco Tuesday) & a modern jukebox.",
+    author: "Melody T.",
     date: "2015-02-13T17:57:28.556094Z"
     },
     {
     id: 13,
     dishId: 2,
-    rating: 4,
-    comment: "Ultimate, Reaching for the stars!",
-    author: "Ringo Starry",
+    rating: 5,
+    comment: "Scratch cocktail for $7. Not too boozy (The Back Door is good for that) and delicious. Food bites (did not have any, but it looked great). Free peanuts. Salty and good. What I have liked the most is that it was not busy when I was there, but both bartenders worked the entire time. Jeremy was friendly, drinks were perfectly balanced, and he never missed a beat when my glass was getting emptier. I really appreciated his attention to detail when he opened a new bag of peanuts. He made sure that the ones that were on the bottom ended up on top. It seems funny to point that out, but it is telling on how the bar is handled in general.  Welcome Double J Saloon to LCW!",
+    author: "Lejla M.",
     date: "2013-12-02T17:57:28.556094Z"
     },
     {
     id: 14,
     dishId: 2,
-    rating: 2,
-    comment: "It's your birthday, we're gonna party!",
-    author: "25 Cent",
+    rating: 4,
+    comment: "Great new addition to LCW! Only been opens ~2 weeks but I'm excited it's here. It's the perfect hangout spot that is comfortable. Great people, big TV's, pool, beers on tap, liquor, music & flatbread pizza (didn't try but smelled good). Parking in the back too. Excited to go back!",
+    author: "Carly C.",
     date: "2011-12-02T17:57:28.556094Z"
     },
     {
     id: 15,
     dishId: 3,
     rating: 5,
-    comment: "Imagine all the eatables, living in conFusion!",
-    author: "John Lemon",
+    comment: "Quaint dive bar ambiance where the food outshines the drink which is a tough feat in itself.  Hats off to THE CHEF- your BBQ beef and pulled pork sando tickled my test buds.  This is a great bar to come to after work with some co-workers and knock a few.  Surprisingly delighted.",
+    author: "Jen M.",
     date: "2012-10-16T17:57:28.556094Z"
     },
     {
     id: 16,
     dishId: 3,
-    rating: 4,
-    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-    author: "Paul McVites",
+    rating: 5,
+    comment: "This place is a great dive bar. They have the best ribs in town. I have had them without bbq sauce because they are so so moist. They have great bbq sauce 3 different KC style and a Carolina if you so choose",
+    author: "Malachy S.",
     date: "2014-09-05T17:57:28.556094Z"
     },
     {
     id: 17,
     dishId: 3,
     rating: 3,
-    comment: "Eat it, just eat it!",
-    author: "Michael Jaikishan",
+    comment: "Great staff! I got stuck in the parking lot next to this place and someone came out and helped us out. I love you guys",
+    author: "Kristen V.",
     date: "2015-02-13T17:57:28.556094Z"
     },
     {
     id: 18,
     dishId: 3,
-    rating: 4,
-    comment: "Ultimate, Reaching for the stars!",
-    author: "Ringo Starry",
+    rating: 5,
+    comment: "This place is my jam, refuge, local hang, and all that really good stuff in between. I've been delaying writing this review because I love the fact stumbling upon Double J Saloon and feeling like I've discovered a super cool secret. The music slash playlist is always on point. The bartenders are also super friendly. They always try to make a point in learning my name and trying to remember it. They also provide some pretty sweet board games. You can easily play chess, while simultaneously playing Jenga and scrabble- whoa.  Don't judge a book by its cover because the interior is super clean, bathrooms are really nice, and the drinks - my personal fav the cucumber cooler never disappoints!",
+    author: "Jessica S.",
     date: "2013-12-02T17:57:28.556094Z"
     },
     {
     id: 19,
     dishId: 3,
-    rating: 2,
-    comment: "It's your birthday, we're gonna party!",
-    author: "25 Cent",
+    rating: 4,
+    comment: "Good, neighborhood place in the thick of Lake City Way. Delicious beverages! Ask about their shot-and-beer combinations. Friendly staff. Tacos, chili, other pub food. Nice, down home, real people. Pool table. Cool decorations, too.",
+    author: "Jenn S.",
     date: "2011-12-02T17:57:28.556094Z"
     }
 ];
