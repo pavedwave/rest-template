23cba9240872ff31e7f3bd0bc79e8dd63e80e41e
pavedwave <pavedwave@gmail.com>
2019-02-02 07:35:31 -0800

    Assign3_PreTask3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -49,8 +49,7 @@ function About(props) {
             <div className="row row-content">
                 <div className="col-12 col-md-6">
                     <h2>Our History</h2>
-                    <p>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</p>
-                    <p>The restaurant traces its humble beginnings to <em>The Frying Pan</em>, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</p>
+                    <p>Brief history of the Double J Saloon - interview with Jeff Flood - placeholder for now.</p>
                 </div>
                 <div className="col-12 col-md-5">
                     <Card>
@@ -60,11 +59,11 @@ function About(props) {
                                 <dt className="col-6">Started</dt>
                                 <dd className="col-6">3 Feb. 2013</dd>
                                 <dt className="col-6">Major Stake Holder</dt>
-                                <dd className="col-6">HK Fine Foods Inc.</dd>
+                                <dd className="col-6">Double J Saloon</dd>
                                 <dt className="col-6">Last Year's Turnover</dt>
                                 <dd className="col-6">$1,250,375</dd>
                                 <dt className="col-6">Employees</dt>
-                                <dd className="col-6">40</dd>
+                                <dd className="col-6">4</dd>
                             </dl>
                         </CardBody>
                     </Card>
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Card, CardTitle, CardText, CardImg, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardTitle, CardText, CardImg, Breadcrumb, BreadcrumbItem, Nav, NavItem, Button, 
+    Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input, Col, Navbar, NavbarToggler, Collapse } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 function RenderComments({comments}) {
 
@@ -20,10 +22,109 @@ function RenderComments({comments}) {
                 <ul>
                     {commentDishes}
                 </ul>
+                <CommentForm />
             </div>
         );
     } else {
         return <div></div>;
+    }
+}
+
+// https://fontawesome.com/v4.7.0/examples/
+
+class CommentForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state= {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.createComment = this.createComment.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    createComment(event) {
+        this.toggleModal();
+        alert("Rating: " + this.rating.value + "Yourname: " + this.yourname.value + " Comment: " + this.comment.value);
+        event.preventDefault();
+    }
+
+    render() {  
+        return(
+            <React.Fragment>
+
+                <Navbar dark expand="md">
+                    <div className="container">
+                    <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <Nav className="ml-auto" navbar>
+                                    <NavItem>
+                                        <Button onClick={this.toggleModal}>
+                                            <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                                        </Button>
+                                    </NavItem>
+                                </Nav>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment Form</ModalHeader>
+                    <ModalBody>
+
+                        <Form onSubmit={this.createComment}>   {/* <== is that benign comment? */}
+
+                        {/* <LocalForm onSubmit={(values) => this.createComment(values)}> */}
+{/*                             <FormGroup>
+                                <Label htmlFor="rating">Rating</Label>
+                                <FormControl as="select">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                <Input type="rating" id="rating" name="rating" 
+                                    innerRef={(input) => this.rating = input} />
+                                </FormControl>
+                            </FormGroup> */}
+
+                            <FormGroup>
+                                <Label htmlFor="yourname">Your Name</Label>
+                                <Input type="text" id="yourname" name="yourname" 
+                                    innerRef={(input) => this.yourname = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="message" md={2}>Comment</Label>
+                                <Col md={10}>
+                                    <Control.textarea model=".message" id="message" name="message" 
+                                        rows="6"
+                                        className="form-control" />
+                                </Col>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="bg-dark">Submit</Button>
+                         </Form>
+                        {/* </LocalForm> */}
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        )
     }
 }
 
